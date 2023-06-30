c6de4e3b18156ab21db22382cb82a9880a6475ff
abdullah201240 <abdullahalsakib7075@gmail.com>
2023-06-30 08:55:59 +0600

    google map and state view

--- app/Http/Controllers/singup_singin.php
+++ app/Http/Controllers/singup_singin.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
-use Session;
+use Illuminate\Support\Facades\Session;
+
 use Response;
 use Illuminate\Support\Facades\Storage;
 use Stevebauman\Location\Facades\Location;
